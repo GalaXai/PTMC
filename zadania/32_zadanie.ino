2f98d6b38d97c0a7f3b01df7bdd69016da46b614
GalaxUniv <afterhoursbilly@gmail.com>
2023-12-13 18:12:35 +0100

    fixed version we increase voltage rather than lowering it

--- zadania/32_zadanie.ino
+++ zadania/32_zadanie.ino
@@ -12,34 +12,34 @@ void setup() {
   lcd.begin(16, 2);  // Set the number of LCD columns and rows
   Serial.begin(9600);  // Start the serial communication
 
-  // Set relay pins as outputs and turn them off
+  // Set relay pins as outputs and turn them on (activate) by default
   for (int i = 0; i < numRelays; i++) {
     pinMode(relayPins[i], OUTPUT);
-    digitalWrite(relayPins[i], LOW);
+    digitalWrite(relayPins[i], LOW);  // Set relays to LOW (deactivated)
   }
 
-  pinMode(currentSensorPin, INPUT); // Set the current sensor pin as input
+  pinMode(currentSensorPin, OUTPUT); // Set the current sensor pin as input
+  // Setup lcd
+  lcd.setCursor(0, 0);  // Set the cursor position
+  lcd.print("Voltage: ");
+  
 }
 
 void loop() {
-  deactivateAllRelays(); 
+  // delay(5000);
   double voltage = readVoltage(A7);  // Read scaled voltage from A7 once per loop
-  // float current = readCurrent(currentSensorPin); // Read the current from the current sensor
- 
 
   // Logic to activate relays based on the voltage
-  if (voltage == 5.0) {
-    int i = 0;
-    while (voltage == 5.0 && i < numRelays) {
+  if (voltage < 2.5) {
+    for (int i = 7; 2.5 > readVoltage(A7); i--) {
       activateRelay(i);
-		 delay(500);  // Delay to allow the voltage to settle
-      voltage = readVoltage(A7);  // Re-read the voltage
-      i++;  // Increment the relay counter
+		 delay(1000);
     }
-	double scaledVoltage = voltage * pow(2, i);
-	printVoltage(scaledVoltage);	
+    double scaledVoltage = voltage * pow(2, numRelays);
+    printVoltage(scaledVoltage);
   } else {
-    deactivateAllRelays();  // If voltage is too low, turn off all relays
+	delay(1000);    
+  deactivateAllRelays();  // If voltage is high enough, turn off all relays
   }
   delay(1000);  // Wait for 1 second before next loop iteration
 }
@@ -58,11 +58,11 @@ void printVoltage(float voltage) {
 }
 
 void activateRelay(int relayIndex) {
-  digitalWrite(relayPins[relayIndex], LOW);  // Turn on the selected relay
+  digitalWrite(relayPins[relayIndex], HIGH);  // Activate the selected relay
 }
 
 void deactivateAllRelays() {
   for (int i = 0; i < numRelays; i++) {
-    digitalWrite(relayPins[i], HIGH);  // Turn off all relays
+    digitalWrite(relayPins[i], LOW);  // Deactivate all relays
   }
 }
